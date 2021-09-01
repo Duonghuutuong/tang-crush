d3617a4b3fbc20b46e30075a1cf38d49ff3f1828
Duonghuutuong <89891580+Duonghuutuong@users.noreply.github.com>
2021-09-01 16:00:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messlink:https://www.facebook.com/tuong.duonghuu.355 
+    linkfb:https://www.facebook.com/tuong.duonghuu.355 
 }
